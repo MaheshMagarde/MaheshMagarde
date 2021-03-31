d61012e67a059aecc1a4713f25190f0f70192d87
Mahesh Magarde <maheshmagarde1234@gmail.com>
2021-03-31 17:35:29 +0530

    Create Inharitance class

--- Mayur.py
+++ Mayur.py
@@ -1,1 +1,18 @@
-print('Hello')
+class hello:
+    def __init__(self,m1,m2,m3):
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+    def avg(self):
+        self.a = self.m1
+        self.b = self.m1+self.m2
+        self.c = self.m1+self.m2+self.m3
+        return (self.a+self.b+self.c)/3
+class Avg(hello):
+    def Avg(self):
+        pass
+
+
+
+
+
