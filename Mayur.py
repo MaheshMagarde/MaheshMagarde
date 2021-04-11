4b9c3d84ce59103db59b3c9896be9903581baf69
Mahesh Magarde <maheshmagarde1234@gmail.com>
2021-04-11 05:56:24 +0530

    New code Edited From Hacker rank

--- Mayur.py
+++ Mayur.py
@@ -1,3 +1,4 @@
+'''
 class hello:
     def __init__(self,m1,m2,m3):
         self.m1 = m1
@@ -12,7 +13,37 @@ class Avg(hello):
     def Avg(self):
         pass
     mahesh
+'''
+'''
+from HTMLParser import HTMLParser
+import _markupbase
+n = int(input())
+s = []
+for i in range(n):
+    a = input()
+    s.append(a)
+k = ''
+for j in s:
+    k += j
 
+
+class MyHTMLParser1(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start :", tag)
+        for attr in attrs:
+            print(f'-> {attr[0]} > {attr[1]} ')
+
+    def handle_endtag(self, tag):
+        print("End   :", tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print("Empty :", tag)
+
+
+
+parser = MyHTMLParser1()
+parser.feed(k)
+'''
 
 
 
