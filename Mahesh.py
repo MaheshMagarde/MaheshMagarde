94e344cbabeced35650b083b67bf28809daa0fbc
Mahesh Magarde <maheshmagarde1234@gmail.com>
2021-05-08 05:47:51 +0530

    not imp

--- Mahesh.py
+++ Mahesh.py
@@ -5,8 +5,3 @@ my_arr, my_arr1 = np.array([[1,2,3],
                             [7,8,9]],np.int32), np.array([[1,5,3],
                                                           [7,3,6],
                                                           [7,8,7]],np.int32)
-
-print( my_arr1.T,',','\n',my_arr.T)
-
-
-mahesh
